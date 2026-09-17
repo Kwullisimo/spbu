159f33399158eb491a87cbebe0ae6f8702fbb2e7
Kwullisimo <rodinov0390@gmail.com>
2026-09-18 00:37:46 +0300

    initional body

--- snail/main.c
+++ snail/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
+// minimum before last day (h-a) / vector (a-b) + 1 (guaranteed last day)
 // ebanoe rounding up a/b = (a+b-1)/b
 
 int main(int argc, char* argv)
 {
     int h, a, b;
     scanf("%d %d %d", &h, &a, &b);
-    printf("%d", (h-b-1)/(a-b) + 1);
+    printf("%d", (h-b-1)/(a-b) + 1); 
     return 0;
 } 
